62c1dd0066415b429134aa7d79006c42a54946a3
Theophilus <extheo2019@gmail.com>
2021-11-20 23:18:26 +0100

    Redesigned portfolio site starting with the header

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -3,19 +3,19 @@ const navToggle = document.getElementById("nav-toggle");
 const navMenu = document.querySelector(".nav__menu");
 
 // Toggle Nav
-navToggle.addEventListener('click', () => {
-    navToggle.classList.toggle('fa-times');
-    navMenu.classList.toggle('show');
-})
+navToggle.addEventListener("click", () => {
+  navToggle.classList.toggle("fa-times");
+  navMenu.classList.toggle("show");
+});
 
 // Active Link
-function linkAction () {
-    // Active link
-    navLink.forEach(n => n.classList.remove('active'));
-    this.classList.add('active');
+function linkAction() {
+  // Active link
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
 
-    navMenu.classList.remove('show');
-    
+  navMenu.classList.remove("show");
+  navToggle.classList.remove("fa-times");
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
